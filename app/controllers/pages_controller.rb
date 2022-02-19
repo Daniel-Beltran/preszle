0b43e58228bd121221b3dde533d80661e5e14437
Max Cano <max.cano1999@gmail.com>
2022-02-19 16:46:16 +0100

    migration issues

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    n = Articles.new("7b0920db76484943a73573fee21b80de")
-    n.get_everything(q: "apple", from: "2018-01-05&to=2018-01-05", sortBy: "popularity")
-
   end
-
 
 end
