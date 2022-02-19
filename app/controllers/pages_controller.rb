9d6c9ababd259d96ee2e1c37f6536158fe8080dc
Max Cano <max.cano1999@gmail.com>
2022-02-19 16:17:24 +0100

    working on implementing News API

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    n = Articles.new("7b0920db76484943a73573fee21b80de")
+    n.get_everything(q: "apple", from: "2018-01-05&to=2018-01-05", sortBy: "popularity")
+
   end
+
+
 end
