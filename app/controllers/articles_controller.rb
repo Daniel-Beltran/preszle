f3bf6e5dc71d934c01e7d905c4d8ed025e4f1dff
Max Cano <max.cano1999@gmail.com>
2022-02-22 10:15:54 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  require 'news-api'
   def index
     @n = News.new("7b0920db76484943a73573fee21b80de")
-    @n.get_top_headlines(sources: "bbc-news")
+    @x = @n.get_everything(q: "russia", from: "2022-02-20&to=2022-02-22", sortBy: "popularity")
   end
 end
