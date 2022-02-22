bf08fd81954d2c3e94864a375b30e9be2c3156eb
Max Cano <max.cano1999@gmail.com>
2022-02-22 21:01:15 +0100

    only 3 articles being displayed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
   require 'news-api'
   def index
-    @n = News.new("7b0920db76484943a73573fee21b80de")
-    @x = @n.get_everything(q: "crypto", from: "2022-02-10&to=2022-02-22", sortBy: "popularity")
+    @y = []
+    @news = News.new("7b0920db76484943a73573fee21b80de")
+    @x = @news.get_everything(q: "spain", from: "2022-01-25&to=2022-02-22", sortBy: "popularity", pageSize: 3)
   end
 end
