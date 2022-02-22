2c971a3fba0ec6b4f3c2eb331fb1769640519578
Max Cano <max.cano1999@gmail.com>
2022-02-22 19:22:10 +0100

    working on news api implementation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,6 @@ class ArticlesController < ApplicationController
   require 'news-api'
   def index
     @n = News.new("7b0920db76484943a73573fee21b80de")
-    @x = @n.get_everything(q: "russia", from: "2022-02-20&to=2022-02-22", sortBy: "popularity")
+    @x = @n.get_everything(q: "crypto", from: "2022-02-10&to=2022-02-22", sortBy: "popularity")
   end
 end
