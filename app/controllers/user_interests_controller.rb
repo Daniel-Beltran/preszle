2b4a5e90308048096869cea7901d86c30f228a17
Daniel Beltran <danfbg@gmail.com>
2022-02-24 19:42:18 +0100

    m

--- app/controllers/user_interests_controller.rb
+++ app/controllers/user_interests_controller.rb
@@ -2,12 +2,12 @@ class UserInterestsController < ApplicationController
 
   def new 
     @user_interest = UserInterest.new
-    
   end
 
 
   def create
-    
+    @user_interest = UserInterest.new(user_interest_params)
+    @user_interest.user = current_user
   end  
  
 
