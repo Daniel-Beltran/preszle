f31d2ca6092ec8fc33a62d709601604c121b17cd
Max Cano <max.cano1999@gmail.com>
2022-02-24 19:52:15 +0100

    added description to articles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_153901) do
+ActiveRecord::Schema.define(version: 2022_02_24_185029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_02_19_153901) do
     t.bigint "interest_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["interest_id"], name: "index_articles_on_interest_id"
   end
 
