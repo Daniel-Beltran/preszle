1905f644a9f0e91aecc738f4d66e2b716e5b96bf
Wouter Hilderson <wouter.hilderson@gmail.com>
2022-02-24 22:11:44 +0100

    navbar javascript start

---
+++ app/javascript/components/navbarSecondary.js
@@ -0,0 +1,24 @@
+const toggleReadTimeSecondaryNavbar = () => {
+  const readtime = document.querySelector('#readtime');
+  const interests = document.querySelector('#interests');
+  const readlists = document.querySelector('#readlists');
+  const dashboard = document.querySelector('#dashboard');
+  const navbarSecondary = document.querySelector('#navbar-secondary')
+
+  readtime.addEventListener('click', () => {
+    console.log('readtime');
+  });
+  interests.addEventListener('click', () => {
+    console.log('interests');
+  });
+  readlists.addEventListener('click', () => {
+    console.log('readlists');
+  });
+  dashboard.addEventListener('click', () => {
+    console.log('dashboard');
+    navbarSecondary.classList.toggle('d-none');
+    dashboard.classList.toggle('text-info');
+  });
+}
+
+export { toggleReadTimeSecondaryNavbar };
