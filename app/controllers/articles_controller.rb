3aa5cbe5588c9bb3ac55a1a0ada9ed6b49fa4153
Max Cano <max.cano1999@gmail.com>
2022-02-26 11:53:21 +0100

    development version with just 3 articles and unalligned image

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,14 @@ class ArticlesController < ApplicationController
   require 'news-api'
   def index
     @news = []
-    @api_news = News.new(ENV["NEWS_API"])
-    @three_articles = @api_news.get_everything(q: "blockchain", searchIn: "title",
-                                               from: "2022-01-25&to=2022-02-22", sortBy: "popularity", pageSize: 3)
-    @three_articles.each do |n|
-      @news << (Article.create title: n.title, description: n.description, source_url: n.url)
+    #@api_news = News.new(ENV["NEWS_API"])
+    #@three_articles = @api_news.get_everything(q: "blockchain", searchIn: "title",
+    #                                           from: "2022-01-25&to=2022-02-22", sortBy: "popularity", pageSize: 3)
+    #@three_articles.each do |n|
+      #@news << (Article.create title: n.title, description: n.description, source_url: n.url, image: n.urlToImage)
+    #end
+    3.times do
+      @news << ((Article.create title: "Article", description: "Here goes the description", source_url: "https://en.wikipedia.org/wiki/Pretzel", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/BrezelnSalz02_%28cropped%29.JPG/375px-BrezelnSalz02_%28cropped%29.JPG"))
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_24_185029) do
+ActiveRecord::Schema.define(version: 2022_02_26_095949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_02_24_185029) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
+    t.string "image"
     t.index ["interest_id"], name: "index_articles_on_interest_id"
   end
 
