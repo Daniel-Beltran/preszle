54c896eb3a459adc9417b28d97689ae273ee216a
Max Cano <max.cano1999@gmail.com>
2022-02-26 10:29:35 +0100

    working on index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   def index
     @news = []
     @api_news = News.new(ENV["NEWS_API"])
-    @three_articles = @api_news.get_everything(q: "bitcoin", searchIn: "title",
+    @three_articles = @api_news.get_everything(q: "blockchain", searchIn: "title",
                                                from: "2022-01-25&to=2022-02-22", sortBy: "popularity", pageSize: 3)
     @three_articles.each do |n|
       @news << (Article.create title: n.title, description: n.description, source_url: n.url)
