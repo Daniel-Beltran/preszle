86d5862b29dd93e55fcf4dc5a1978765e0a411ca
Marthe Mukamazimpaka <marthemukam@gmail.com>
2022-03-01 20:24:12 +0100

    edit user profile

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
     @news = []
     #@api_news = News.new(ENV["NEWS_API"])
     #@three_articles = @api_news.get_everything(q: "blockchain", searchIn: "title",
-    #                                           from: "2022-01-25&to=2022-02-22", sortBy: "popularity", pageSize: 3)
+    #                                           from: "2022-02-25&to=2022-03-22", sortBy: "popularity", pageSize: 3)
     #@three_articles.each do |n|
       #@news << (Article.create title: n.title, description: n.description, source_url: n.url, image: n.urlToImage)
     #end
