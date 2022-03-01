fefb96634a3d7958bee812c995358117576f8b2a
Daniel Beltran <danfbg@gmail.com>
2022-03-01 20:10:09 +0100

    m

--- app/controllers/user_interests_controller.rb
+++ app/controllers/user_interests_controller.rb
@@ -1,23 +1,25 @@
 class UserInterestsController < ApplicationController
 
   def index
+    new
+  end
+
+  def new
     @user_interests = current_user.interests
     @interests = Interest.all
-    @updated_interests=[]
-    @no_interests=[]
-  end  
-
-  def new 
-    @user_interest = UserInterest.new
   end
 
   def create
-    interest_ids = params[:user_interest][:interest]
-    interest_ids.delete("")
+    interest_ids = params[:user_interest].keys
+    current_user.interests.clear
     interest_ids.each do |id|
-      current_user.interests << Interest.find(id)
+      if params[:user_interest][id] == "1"
+        current_user.interests << Interest.find(id.to_i)
+      end  
     end
-  end  
- 
-
+  end
 end
+
+ # interest_ids.delete("")
+ # interest_ids.each do |id|
+   # current_user.interests << Interest.find(id)
