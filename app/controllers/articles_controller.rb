09311f6747484f2e79b3c84ea02ea76d990ee530
Max Cano <max.cano1999@gmail.com>
2022-03-01 20:36:59 +0100

    show starting to work

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,13 +4,17 @@ class ArticlesController < ApplicationController
     @news = []
     #@api_news = News.new(ENV["NEWS_API"])
     #@three_articles = @api_news.get_everything(q: "blockchain", searchIn: "title",
-    #                                           from: "2022-01-25&to=2022-02-22", sortBy: "popularity", pageSize: 3)
+                                               #from: "2022-02-25&to=2022-03-01", sortBy: "popularity", pageSize: 3)
     #@three_articles.each do |n|
-      #@news << (Article.create title: n.title, description: n.description, source_url: n.url, image: n.urlToImage, source: n.source)
+      #@news << (Article.create title: n.title, description: n.description, source_url: n.url, image: n.urlToImage)
     #end
     3.times do
-      @news << ((Article.create title: "Article", description: "Here goes the description", source: "Article Source", reading_time: 15,
-                 source_url: "https://en.wikipedia.org/wiki/Pretzel", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/BrezelnSalz02_%28cropped%29.JPG/375px-BrezelnSalz02_%28cropped%29.JPG"))
+      @news << ((Article.create! title: "Article", description: "Here goes the description", source: "Article Source", reading_time: 15,
+                 source_url: "https://en.wikipedia.org/wiki/Pretzel", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/BrezelnSalz02_%28cropped%29.JPG/375px-BrezelnSalz02_%28cropped%29.JPG", author: "Its a me", content:"Mario", interest_id: 1))
     end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
