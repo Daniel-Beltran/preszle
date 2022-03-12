ae0feb74b3a9b31edd0e920b412a758bad3fdb25
Max Cano <max.cano1999@gmail.com>
2022-03-12 11:26:11 +0100

    working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,18 @@
 class ArticlesController < ApplicationController
   require 'news-api'
-
-#grab articles from the DB where the interest id matches the interest id of the current user
-
   def index
     @news = []
     @interests_array = []
     current_user.interests.each do |i|
       @interests_array << i.name
     end
-
     Article.all.each do |n|
-    if n.interest_id == current_user.interests[0].id
-      @news << n
+      (0...current_user.interests.count).each do |i|
+        if n.interest_id == current_user.interests[i].id
+          @news << n
+        end
+      end
     end
-  end
-
-
-
-#SAVING AND DISPLAYING 3 PLACEHOLDERS
-    #3.times do
-    #   @news << ((Article.create! title: "Article", description: "Here goes the description", source: "Article Source", reading_time: 15,
-    #             source_url: "https://en.wikipedia.org/wiki/Pretzel", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/BrezelnSalz02_%28cropped%29.JPG/375px-BrezelnSalz02_%28cropped%29.JPG", author: "Its a me", content:"Mario", interest_id: 1))
-    #end
   end
 
   def show
